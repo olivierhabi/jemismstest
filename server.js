7fa93133b19313b4a5c4a7b98ec2facb675a8b34
HABIMANA Olivier <habimanaolivier6@gmail.com>
2019-07-29 23:17:31 -0700

    Themes Added

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-const express = require ('express');
-const hbs = require('hbs');
-const fs = require('fs');
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
 
 const port = process.env.PORT || 3000;
 var app = express();
 
-
-hbs.registerPartials(__dirname + '/views/partials');
-app.set('view enginse', 'hbs');
-
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view enginse", "hbs");
 
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
+  fs.appendFile("server.log", log + "\n", err => {
     if (err) {
-      console.log('Unable to append to server.log')
+      console.log("Unable to append to server.log");
     }
   });
   next();
@@ -27,75 +25,84 @@ app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
 // });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear()
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
 });
 
-
-app.get('/', (req, res) => {
-  res.render('my_account.hbs', {
+app.get("/", (req, res) => {
+  res.render("my_account.hbs", {
     pageTitle: "My Account",
     welcomeText: "Welcome to Our site"
   });
 });
 
-app.get('/profile', (req, res) => {
-  res.render('profile.hbs', {
-    pageTitle: 'My Profile'
+app.get("/profile", (req, res) => {
+  res.render("profile.hbs", {
+    pageTitle: "My Profile"
   });
 });
 
-app.get('/contacts', (req, res) => {
-  res.render('contacts.hbs', {
-    pageTitle: 'My Contacts'
+app.get("/index", (req, res) => {
+  res.render("index.hbs", {
+    pageTitle: "Index Page"
   });
 });
 
-app.get('/groups', (req, res) => {
-  res.render('groups.hbs', {
-    pageTitle: 'Mys Groups'
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard.hbs", {
+    pageTitle: "Dashboard Page"
   });
 });
 
-app.get('/orders', (req, res) => {
-  res.render('orders.hbs', {
-    pageTitle: 'My Orders'
+app.get("/contacts", (req, res) => {
+  res.render("contacts.hbs", {
+    pageTitle: "My Contacts"
   });
 });
 
-app.get('/send', (req, res) => {
-  res.render('send.hbs', {
-    pageTitle: 'Send'
+app.get("/groups", (req, res) => {
+  res.render("groups.hbs", {
+    pageTitle: "Mys Groups"
   });
 });
 
-app.get('/custom', (req, res) => {
-  res.render('custom.hbs', {
-    pageTitle: 'Mys Groups'
+app.get("/orders", (req, res) => {
+  res.render("orders.hbs", {
+    pageTitle: "My Orders"
   });
 });
 
-app.get('/sent', (req, res) => {
-  res.render('sent.hbs', {
-    pageTitle: 'Sent'
+app.get("/send", (req, res) => {
+  res.render("send.hbs", {
+    pageTitle: "Send"
   });
 });
 
-app.get('/scheduled', (req, res) => {
-  res.render('scheduled.hbs', {
-    pageTitle: 'Scheduled'
+app.get("/custom", (req, res) => {
+  res.render("custom.hbs", {
+    pageTitle: "Mys Groups"
   });
 });
 
-app.get('/bad', (req, res) => {
+app.get("/sent", (req, res) => {
+  res.render("sent.hbs", {
+    pageTitle: "Sent"
+  });
+});
+
+app.get("/scheduled", (req, res) => {
+  res.render("scheduled.hbs", {
+    pageTitle: "Scheduled"
+  });
+});
+
+app.get("/bad", (req, res) => {
   res.send({
-    errorMessage: 'Unable to handle request'
+    errorMessage: "Unable to handle request"
   });
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
